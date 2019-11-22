89a778e5c76ae48c6ce0d3ddd1cb18d0e6bb3189
chenguiquan <String222>
2019-11-22 19:08:02 +0800

    添加docker file

--- mall-mybatis-plus/src/test/java/mapper/ActivityCouponMapperTest.java
+++ mall-mybatis-plus/src/test/java/mapper/ActivityCouponMapperTest.java
@@ -1,41 +1,41 @@
-package mapper;
-
-import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
-import com.jianke.demo.MallMybatisPlusApplication;
-import com.jianke.demo.config.MybatisPlusConfig;
-import com.jianke.demo.entity.ActivityCoupon;
-import com.jianke.demo.mapper.ActivityCouponMapper;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-
-/**
- * @program: mall-share
- * @description: ${description}
- * @author: chenguiquan
- * @create: 2019-08-13 20:46
- **/
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {
-        MallMybatisPlusApplication.class,
-        DataSourceAutoConfiguration.class,
-        MybatisPlusAutoConfiguration.class,
-        MybatisPlusConfig.class
-})
-public class ActivityCouponMapperTest {
-
-    @Autowired
-    private ActivityCouponMapper mapper;
-
-    @Test
-    public void testFindBySettingId(){
-        ActivityCoupon activityCoupon = mapper.selectById(3);
-        assertNotNull(activityCoupon);
-    }
-}
+//package mapper;
+//
+//import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+//import com.jianke.demo.MallMybatisPlusApplication;
+//import com.jianke.demo.config.MybatisPlusConfig;
+//import com.jianke.demo.entity.ActivityCoupon;
+//import com.jianke.demo.mapper.ActivityCouponMapper;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+//
+///**
+// * @program: mall-share
+// * @description: ${description}
+// * @author: chenguiquan
+// * @create: 2019-08-13 20:46
+// **/
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = {
+//        MallMybatisPlusApplication.class,
+//        DataSourceAutoConfiguration.class,
+//        MybatisPlusAutoConfiguration.class,
+//        MybatisPlusConfig.class
+//})
+//public class ActivityCouponMapperTest {
+//
+//    @Autowired
+//    private ActivityCouponMapper mapper;
+//
+//    @Test
+//    public void testFindBySettingId(){
+//        ActivityCoupon activityCoupon = mapper.selectById(3);
+//        assertNotNull(activityCoupon);
+//    }
+//}
